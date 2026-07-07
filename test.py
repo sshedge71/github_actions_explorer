62b081b15cd9be88e6fc4cc4834ce28af5701c91
Suraj Shedge <sshedge@qti.qualcomm.com>
2026-07-07 13:27:52 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
-Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+Copyright (c) suraj shedge
 SPDX-License-Identifier: BSD-3-Clause-Clear
 
 
 # this is just a test file.
+Qualcomm Technologies, Inc. and/or its subsidiaries.
